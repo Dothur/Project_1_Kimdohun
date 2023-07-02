4469c08073e2203b0e293c7f4aa4562a1053bf18
dothur <craz1000@naver.com>
2023-07-03 02:21:54 +0900

    :art: fix. @column(nullable = false) - 제목, 설명, 최소 가격, 작성자

--- src/main/java/com/example/miniProject/entity/SalesItemEntity.java
+++ src/main/java/com/example/miniProject/entity/SalesItemEntity.java
@@ -10,11 +10,23 @@ public class SalesItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String description;
+
     private String imageUrl;
+
+    @Column(nullable = false)
     private Integer minPriceWanted;
+
     private String status;
+
+    @Column(nullable = false)
     private String writer;
+
+    @Column(nullable = false)
     private String password;
 }
