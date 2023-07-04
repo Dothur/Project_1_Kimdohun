41f5630fa2c185d2748b3abc1409e85eecde556b
dothur <craz1000@naver.com>
2023-07-04 15:02:33 +0900

    :art: fix. @Column nullable 속성 추가

--- src/main/java/com/example/miniProject/entity/NegotiationEntity.java
+++ src/main/java/com/example/miniProject/entity/NegotiationEntity.java
@@ -10,9 +10,13 @@ public class NegotiationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private Long itemId;
+    @Column(nullable = false)
     private Integer suggestedPrice;
     private String status;
+    @Column(nullable = false)
     private String writer;
+    @Column(nullable = false)
     private String password;
 }
