dca7ffac4811a7eebd175856932ef4926982a53d
dothur <craz1000@naver.com>
2023-07-27 16:01:44 +0900

    :wrench: fix. JwtTokenFilter 등록, requestMatchers 수정

--- src/main/java/com/example/miniProject/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/miniProject/auth/config/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.miniProject.auth.config;
 
+import com.example.miniProject.auth.jwt.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+    private final JwtTokenFilter jwtTokenFilter;
+
+    public WebSecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
 
     @Bean // 메소드의 결과를 Bean 객체로 등록해주는 어노테이션
     public SecurityFilterChain securityFilterChain(
@@ -27,13 +35,14 @@ public class WebSecurityConfig {
                                 // requestMatchers == 어떤 URL로 오는 요청에 대하여 설정하는지
                                 // permitAll() == 누가 요청해도 허가한다.
                                 .requestMatchers(
-                                        "/",
-                                        "/users/login"
+                                        "/no-auth",
+                                        "/users/login",
+                                        "/token/issue"
                                 )
                                 .permitAll()
 
                                 .requestMatchers(
-                                        "/no-auth",
+                                        "/",
                                         "/users/register"
                                 )
                                 .anonymous() // 인증이 되지 않은 사용자만 허가
@@ -41,37 +50,51 @@ public class WebSecurityConfig {
                                 .requestMatchers(
                                         "re-auth", // ex
                                         "/users/my-profile",
-                                        "users/logout"
+                                        "/users/logout",
+                                        "/token/secured"
                                 )
                                 .authenticated() // 인증이 된 사용자만 허가
                 )
-                // form 을 이용한 로그인 관련 설정
-                .formLogin(
-                        formLogin -> formLogin
-                                // 로그인 하는 페이지 지정
-                                .loginPage("/users/login")
-                                // 성공시 이동하는 페이지
-                                .defaultSuccessUrl("/users/my-profile")
-                                // 실패시 이동하는 페이지
-                                .failureUrl("/users/login?fail")
-                                .permitAll()
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .logout(
-                        logout -> logout
-                                // 로그아웃 요청을 보낼 url
-                                // 어떤 UI에 로그아웃 기능을 연결하고 싶으면
-                                // 해당 UI가 /users/logout 으로 POST 요청을 보내게끔
-                                .logoutUrl("/users/logout")
-                                // 성공시 이동하는 url
-                                .logoutSuccessUrl("/users/login")
+                .addFilterBefore(
+                        jwtTokenFilter,
+                        AuthorizationFilter.class
                 )
         ;
+//                // form 을 이용한 로그인 관련 설정
+//                .formLogin(
+//                        formLogin -> formLogin
+//                                // 로그인 하는 페이지 지정
+//                                .loginPage("/users/login")
+//                                // 성공시 이동하는 페이지
+//                                .defaultSuccessUrl("/users/my-profile")
+//                                // 실패시 이동하는 페이지
+//                                .failureUrl("/users/login?fail")
+//                                .permitAll()
+//                )
+//                .logout(
+//                        logout -> logout
+//                                // 로그아웃 요청을 보낼 url
+//                                // 어떤 UI에 로그아웃 기능을 연결하고 싶으면
+//                                // 해당 UI가 /users/logout 으로 POST 요청을 보내게끔
+//                                .logoutUrl("/users/logout")
+//                                // 성공시 이동하는 url
+//                                .logoutSuccessUrl("/users/login")
+//                )
+//        ;
         return http.build();
     }
 
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    // 비밀번호 암호화를 위한 Bean
+    public PasswordEncoder passwordEncoder(){
+        // 기본적으로 사용자 비밀번호는 해독가능한 형태로 데이터베이스에
+        // 저장되면 안된다. 그래서 기본적으로 비밀번호를 단방향 암호화 하는
+        // 인코더를 사용한다.
         return new BCryptPasswordEncoder();
     }
 }
